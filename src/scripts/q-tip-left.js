15b6e85f1488a912823228c49a3c4b4f9e82c142
Eric Lathrop <eric@ericlathrop.com>
2016-01-31 16:44:58 -0500

    Track success/failure

--- src/scripts/q-tip-left.js
+++ src/scripts/q-tip-left.js
@@ -5,5 +5,10 @@ module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
 	var slideX = game.entities.get(entity, "slideX");
 	delete slideX.onLeft;
 	slideX.onRight = "./scripts/q-tip-right";
+	slideX.max = 300;
 
+	var image = game.entities.get(entity, "image");
+	image.name = "ear-man.png";
+	image.destinationY -= 117;
+	image.destinationHeight = 434;
 };
