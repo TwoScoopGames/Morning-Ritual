9a6745d11dc8ee4e5276bcd61a811bec72272d49
Eric Lathrop <eric@ericlathrop.com>
2016-01-31 11:34:29 -0500

    Add basic flow to feed cat game

---
+++ src/scripts/feed-cat-outro.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
+	game.entities.get(entity, "timers").outro.running = true;
+	game.entities.set(entity, "outro", true);
+	console.log("outro");
+	game.entities.find("failure").forEach(function() {
+	});
+};
