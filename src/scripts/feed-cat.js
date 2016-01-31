8cfb5bdad7e7035a442f32617828eab27cca30a3
Eric Lathrop <eric@ericlathrop.com>
2016-01-31 12:31:05 -0500

    Feed the cat

--- src/scripts/feed-cat.js
+++ src/scripts/feed-cat.js
@@ -5,4 +5,15 @@ module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
 	timers.unrotateFood.running = true;
 	timers.unrotateFood.time = 0;
 	game.entities.get(entity, "rotation").angle = 1.0;
+	var foodCount = game.entities.find("catFood").length;
+	var face = game.entities.find("catFace")[0];
+	if (foodCount === 0) {
+		game.instantiatePrefab("catFood1");
+	} else if (foodCount === 1) {
+		game.instantiatePrefab("catFood2");
+		game.entities.get(face, "image").name = "feed-catFace2.png";
+	} else if (foodCount === 2) {
+		game.instantiatePrefab("catFood3");
+		game.entities.get(face, "image").name = "feed-catFace3.png";
+	}
 };
