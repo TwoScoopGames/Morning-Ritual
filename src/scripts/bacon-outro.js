507ff6d4d45d52e1bf9d5ca438c6d03349ea20d1
Eric Lathrop <eric@ericlathrop.com>
2016-01-31 18:54:20 -0500

    Add bacon sounds

--- src/scripts/bacon-outro.js
+++ src/scripts/bacon-outro.js
@@ -5,5 +5,7 @@ module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
 	game.entities.set(entity, "outro", true);
 	game.entities.find("failure").forEach(function(id) {
 		game.entities.get(id, "image").name = "bacon-pan3.png";
+		game.sounds.stop("bacon");
+		game.sounds.play("oh-no");
 	});
 };
