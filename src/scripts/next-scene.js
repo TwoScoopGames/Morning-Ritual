b11b9bbba3863d9ebe1827581d9162da973d039e
Eric Lathrop <eric@ericlathrop.com>
2016-01-31 11:24:41 -0500

    Switch to random game

--- src/scripts/next-scene.js
+++ src/scripts/next-scene.js
@@ -1,7 +1,17 @@
 "use strict";
 
+var games = [
+	"crack-eggs",
+	"feed-cat"
+];
+
+function randomItem(array) {
+	var i = Math.floor(Math.random() * (array.length + 1));
+	return array[i];
+}
+
 module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
 	var failures = game.entities.find("failure").length > 0;
 	console.log("is failure?", failures);
-	game.switchScene("crack-eggs");
+	game.switchScene(randomItem(games));
 };
