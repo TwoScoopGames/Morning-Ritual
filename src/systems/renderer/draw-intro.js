0412b871dde4c3f394da1ab5e066dacfe7f4c0bf
Alex Bezuska <abezuska@gmail.com>
2016-01-31 11:35:54 -0500

    update intro text

--- src/systems/renderer/draw-intro.js
+++ src/systems/renderer/draw-intro.js
@@ -5,8 +5,10 @@ module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
 	ecs.addEach(function(entity, context) { // eslint-disable-line no-unused-vars
 		var intro = game.entities.get(entity, "intro");
 		var position = game.entities.get(entity, "position");
+		context.fillStyle = "rgba(0,0,0,0.7)";
+		context.fillRect(0,0,1536, 2048);
 		context.font = "120px sans-serif";
-		context.fillStyle = "green";
+		context.fillStyle = "white";
 		context.fillText(intro, position.x, position.y);
 	}, "draw-intro");
 };
