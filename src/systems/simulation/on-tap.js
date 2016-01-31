474e92150f89d2527e32a6f37c3ffef1d92670bb
Eric Lathrop <eric@ericlathrop.com>
2016-01-30 21:43:41 -0500

    Add system for clicking on stuff

---
+++ src/systems/simulation/on-tap.js
@@ -0,0 +1,24 @@
+"use strict";
+
+module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
+	game.entities.registerSearch("apply-on-tap", ["onTap", "position", "size"]);
+	ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
+		var camera = game.entities.find("camera")[0];
+		var cameraPosition = game.entities.get(camera, "position");
+
+		var position = game.entities.get(entity, "position");
+		var size = game.entities.get(entity, "size");
+		var mx = game.input.mouse.x + cameraPosition.x;
+		var my = game.input.mouse.y + cameraPosition.y;
+		if (game.input.buttonPressed("action")
+				&& mx >= position.x
+				&& mx < position.x + size.width
+				&& my >= position.y
+				&& my < position.y + size.height
+				) {
+			var onTap = game.entities.get(entity, "onTap");
+			var script = game.require(onTap.script);
+			script(entity, game);
+		}
+	}, "apply-on-tap");
+};
