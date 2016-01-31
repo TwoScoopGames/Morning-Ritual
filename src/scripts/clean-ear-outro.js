f2f13e8870a3874e7262670f5a276d94d0cc4cc4
Eric Lathrop <eric@ericlathrop.com>
2016-01-31 16:56:16 -0500

    Add fail wax

--- src/scripts/clean-ear-outro.js
+++ src/scripts/clean-ear-outro.js
@@ -4,5 +4,6 @@ module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
 	game.entities.get(entity, "timers").outro.running = true;
 	game.entities.set(entity, "outro", true);
 	game.entities.find("failure").forEach(function() {
+		game.instantiatePrefab("earWax");
 	});
 };
