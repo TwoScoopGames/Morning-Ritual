79a214d026674893c3c9bea002a8a36d617ecbaa
Eric Lathrop <eric@ericlathrop.com>
2016-01-30 22:12:28 -0500

    Cr4ck sum eggz yo

--- src/scripts/remove-failure.js
+++ src/scripts/remove-failure.js
@@ -1,5 +1,9 @@
 "use strict";
 
-module.exports = function(game) { // eslint-disable-line no-unused-vars
-	console.log("remove failure");
+module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
+	game.entities.remove(entity, "failure");
+	game.entities.remove(entity, "onTap");
+	game.entities.set(entity, "image", {
+		"name": "egg-cracked.png"
+	});
 };
