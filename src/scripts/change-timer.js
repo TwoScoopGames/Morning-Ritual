fd4ea068b16b6daec3f3dd8300c253d1eb68bf85
Eric Lathrop <eric@ericlathrop.com>
2016-01-31 17:36:48 -0500

    Add alarm animation

--- src/scripts/change-timer.js
+++ src/scripts/change-timer.js
@@ -4,9 +4,18 @@ module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
 	var gameTimers = game.entities.get(2, "timers");
 	var frame = Math.floor(gameTimers.game.time / gameTimers.game.max * 7) + 1;
 
-	if (frame < 6) {
+	if (frame < 7) {
 		game.entities.get(entity, "timers").changeTimer.running = true;
 		game.entities.get(entity, "timers").changeTimer.max = Math.floor(gameTimers.game.max / 7);
+	}
+	if (frame === 6) {
+		game.entities.set(entity, "animation", {
+			"time": 0,
+			"frame": 0,
+			"loop": true,
+			"speed": 1,
+			"name": "alarm"
+		});
 	}
 
 	game.entities.get(entity, "image").name = "alarm" + frame + ".png";
