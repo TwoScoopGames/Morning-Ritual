c94afde4bdc693e156497b902e721f35329e2c24
Eric Lathrop <eric@ericlathrop.com>
2016-02-23 22:34:53 -0500

    Add failure image

---
+++ src/scripts/slippers-outro.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
+	game.entities.get(entity, "timers").outro.running = true;
+	game.entities.set(entity, "outro", true);
+	game.entities.find("failure").forEach(function() {
+		game.sounds.play("feed-dog");
+		game.entities.get(8, "image").name = "slippers-left-slipper-bottom-loose.png";
+		game.entities.get(9, "image").name = "slippers-left-slipper-top-loose.png";
+		game.entities.get(11, "image").name = "slippers-right-slipper-bottom-loose.png";
+		game.entities.get(12, "image").name = "slippers-right-slipper-top-loose.png";
+	});
+};
