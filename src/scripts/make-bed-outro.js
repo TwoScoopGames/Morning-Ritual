b0f08519ede3a81fa389ac96a13877f426cf5fa6
Eric Lathrop <eric@ericlathrop.com>
2016-02-23 19:20:47 -0500

    Spawn cats immediately

--- src/scripts/make-bed-outro.js
+++ src/scripts/make-bed-outro.js
@@ -3,12 +3,6 @@
 module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
 	game.entities.get(entity, "timers").outro.running = true;
 	game.entities.set(entity, "outro", true);
-	var win = game.entities.find("failure").length === 0;
-	if (win) {
-		game.instantiatePrefab("bed-cat-1");
-		game.instantiatePrefab("bed-cat-2");
-		game.instantiatePrefab("bed-cat-3");
-	}
 	game.entities.find("failure").forEach(function() {
 		game.sounds.play("oh-no");
 	});
--- src/scripts/stop-dragging.js
+++ src/scripts/stop-dragging.js
@@ -4,4 +4,10 @@ module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
 	game.entities.remove(entity, "drag");
 	game.entities.remove(entity, "snapToCenter");
 	game.entities.remove(entity, "failure");
+	var win = game.entities.find("failure").length === 0;
+	if (win) {
+		game.instantiatePrefab("bed-cat-1");
+		game.instantiatePrefab("bed-cat-2");
+		game.instantiatePrefab("bed-cat-3");
+	}
 };
