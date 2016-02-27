98ce0420ca6a021426277b2018707e70171ea42f
Eric Lathrop <eric@ericlathrop.com>
2016-02-26 23:35:13 -0500

    Add failed image

--- src/scripts/alarm-outro.js
+++ src/scripts/alarm-outro.js
@@ -3,7 +3,14 @@
 module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
 	game.entities.get(entity, "timers").outro.running = true;
 	game.entities.set(entity, "outro", true);
-	game.entities.find("failure").forEach(function() {
+	var failed = game.entities.find("failure").length > 0;
+
+	if (failed) {
 		game.sounds.play("oh-no");
-	});
+		game.entities.find("button").slice().forEach(function(button) {
+			game.entities.destroy(button);
+		});
+		game.entities.destroy(5);
+		game.instantiatePrefab("alarm-melted");
+	}
 };
